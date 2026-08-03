4903f69ffd1d20bd791872fd7cc077dce9fdf3c3
Kawana Keigo <kawana.keigo@gmail.com>
2026-08-03 19:28:31 +0900

    Initialize CreVe Spring Boot project

---
+++ src/test/java/world/creve/CreveWebApplicationTests.java
@@ -0,0 +1,13 @@
+package world.creve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreveWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
